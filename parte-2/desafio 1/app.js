a2f0b295182502408e5cb929ba88ea7d765a6a72
Kauan Constancio <kauanconstancio13@gmail.com>
2024-11-30 21:27:16 -0300

    Desafio concluido

---
+++ parte-2/desafio 1/app.js
@@ -0,0 +1,74 @@
+// Desafio 1 
+// Criar uma função que exibe "Olá, mundo!" no console.
+// (Concluido)
+
+// function olaMundo () {
+//     console.log('Olá, mundo!');
+// }
+
+
+
+
+// Desafio 2
+// Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+// (Concluido)
+
+// function olaNome(nome) {
+//     console.log(`Ola, ${nome}!`);
+// }
+
+// olaNome('Kauan');
+
+
+
+
+// Desafio 3
+// Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+// (Concluido)
+
+// function numeroDobro (numero) {
+//     console.log( numero * 2)
+// }
+
+// numeroDobro( 10 )
+
+
+
+
+// Desafio 4
+// Criar uma função que recebe três números como parâmetros e retorna a média deles.
+// (Concluido)
+
+// function mediaNumeros (numero1, numero2, numero3) {
+//     return (numero1 + numero2 + numero3) / 3
+// }
+
+// let media = mediaNumeros(5, 8, 10); 
+// console.log(media);
+
+
+
+
+// Desafio 5
+// Criar uma função que recebe dois números como parâmetros e retorna o maior deles.\
+// (Concluido)
+
+// function numeroMaior (num1, num2){
+//     return (num1 > num2 ? num1 : num2);
+// }
+
+// let maior = numeroMaior(10, 70);
+// console.log(maior);
+
+
+
+
+// Desafio 6
+//Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+//(Concluido)
+
+// function numeroDobro (num1){
+//     return (num1) * num1
+// }
+// let numero = numeroDobro(10);
+// console.log(numero);
